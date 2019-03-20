7a2c0fd2a3ea55c20db671fb7627785d49b5599a
Andres Virga <Andres.Virga@endava.com>
2019-03-20 16:35:11 -0300

    Initial commit

---
+++ TestSample/TestSample/MyChallenge.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ParametersValidation
+{
+    /// <summary>
+    /// Given a string containing just the characters '(', ')', '{', '}', '[' and ']',
+    /// determine if the input string has valid parentheses.
+    /// </summary>
+    public interface IChallenge
+    {
+        bool Execute(string parameters);
+    }
+
+    public class MyChallenge : IChallenge
+    {
+        public bool Execute(string parameters)
+        {
+            //--Check the parameters using
+            var length = parameters.Length;
+
+            //"(){[]}";
+            // 123456
+            for (int pos = 0; pos < length; pos++)
+            {
+                int nextPos = pos + 1;
+
+                //--Checking "()"
+                if (parameters[pos] == '(' && parameters[nextPos] != ')')
+                {
+                    for (int i = nextPos + 1; i < length; i++)
+                    {
+                        if (parameters[i] == ')')
+                            break;
+
+                        //--If there is no match to any closing parenthesis. return false (is not valid)
+                        return false;
+                    }
+                }
+
+                //--Checking "{}"
+                if (parameters[pos] == '{' && parameters[nextPos] != '}')
+                {
+                    for (int i = nextPos + 1; i < length; i++)
+                    {
+                        if (parameters[i] == '}')
+                            break;
+
+                        //--If there is no match to any closing brackets. return false (is not valid)
+                        return false;
+                    }
+                }
+
+                //--Checking "[]"
+                if (parameters[pos] == '[' && parameters[nextPos] != ']')
+                {
+                    for (int i = nextPos + 1; i < length; i++)
+                    {
+                        if (parameters[i] == ']')
+                            break;
+
+                        //--If there is no match to any closing parenthesis. return false (is not valid)
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+
+    [TestClass]
+    public class ParametersValidationTests
+    {
+        IChallenge challenge;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            challenge = new MyChallenge();
+        }
+
+        [TestMethod]
+        public void ValidParameters()
+        {
+            var input = "(){[]}";
+
+            var result = challenge.Execute(input);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void ValidParametersMix()
+        {
+            var input = "(){[]}(((())))(){}[{(({}[]))}]";
+
+            var result = challenge.Execute(input);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void BadClosing()
+        {
+            var input = "()}[])";
+
+            var result = challenge.Execute(input);
+
+            Assert.IsFalse(result);
+        }
+        [TestMethod]
+        public void IncompleteClosing()
+        {
+            var input = "((()";
+
+            var result = challenge.Execute(input);
+
+            Assert.IsFalse(result);
+        }
+
+    }
+}
