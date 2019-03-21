c32b35f4f7734b206227292fee8db1cf98dbc712
andvirga <andvirga@gmail.com>
2019-03-20 22:01:39 -0300

    Added algorithm to check characters 'closure'

--- TestSample/TestSample/MyChallenge.cs
+++ TestSample/TestSample/MyChallenge.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ParametersValidation
 {
@@ -14,58 +16,89 @@ namespace ParametersValidation
 
     public class MyChallenge : IChallenge
     {
-        public bool Execute(string parameters)
+        /// <summary>
+        /// Contains the corresponding 'closure' char for each 'opener' character.
+        /// </summary>
+        Dictionary<char, char> closingCharsDictionary;
+
+        public MyChallenge()
         {
-            //--Check the parameters using
-            var length = parameters.Length;
-
-            //"(){[]}";
-            // 123456
-            for (int pos = 0; pos < length; pos++)
+            closingCharsDictionary = new Dictionary<char, char>
             {
-                int nextPos = pos + 1;
+                { '{', '}' },
+                { '(', ')' },
+                { '[', ']' }
+            };
+        }
 
-                //--Checking "()"
-                if (parameters[pos] == '(' && parameters[nextPos] != ')')
+        public bool Execute(string str)
+        {
+            var result = this.CheckClosure(str, 0);
+
+            return result;
+        }
+
+        /// <summary>
+        /// Checks recursively if there is a match (), {} or [], if positive deletes the match and keeps evaluating the substring
+        /// </summary>
+        /// <param name="str">Substring to evaluate</param>
+        /// <param name="index">Starting point to evaluate</param>
+        /// <returns>Is Valid</returns>
+        private bool CheckClosure(string str, int index)
+        {
+            bool isValid = true;
+
+            while(str.Length > 1)
+            {
+                //--First we check if the character is closed correctly
+                var match = CheckMatch(str[index], str[index+1]);
+
+                //--If the string length is 2 we have the last substring to check, then if it doesn't match, the whole string is invalid
+                if (str.Length == 2 && match)
                 {
-                    for (int i = nextPos + 1; i < length; i++)
-                    {
-                        if (parameters[i] == ')')
-                            break;
-
-                        //--If there is no match to any closing parenthesis. return false (is not valid)
-                        return false;
-                    }
+                    isValid = true;
+                    break;
+                }
+                else if (str.Length == 2 && !match)
+                {
+                    isValid = false;
+                    break;
                 }
 
-                //--Checking "{}"
-                if (parameters[pos] == '{' && parameters[nextPos] != '}')
+                //--If there is a match (eg: {}), then we remove this substring from the whole string and keeps iterating
+                if (match)
                 {
-                    for (int i = nextPos + 1; i < length; i++)
-                    {
-                        if (parameters[i] == '}')
-                            break;
-
-                        //--If there is no match to any closing brackets. return false (is not valid)
-                        return false;
-                    }
+                    str = str.Remove(index, 2); //--Removing 2 chars (initial and closure).
+                    index = 0;                  //--Restart the index to keep checking the next substring
                 }
-
-                //--Checking "[]"
-                if (parameters[pos] == '[' && parameters[nextPos] != ']')
+                else
                 {
-                    for (int i = nextPos + 1; i < length; i++)
-                    {
-                        if (parameters[i] == ']')
-                            break;
-
-                        //--If there is no match to any closing parenthesis. return false (is not valid)
-                        return false;
-                    }
+                    index += 1;                //--Incremeting the index to keep checking the next substring
+                    CheckClosure(str, index);  //--Calling recursively this function to evaluate the substring
                 }
             }
 
-            return true;
+            return isValid;
+        }
+
+        /// <summary>
+        /// Checks if initial char and final char matches the corresponding closure character
+        /// </summary>
+        /// <param name="initial">Initial Char</param>
+        /// <param name="final">Final Char</param>
+        /// <returns>Match</returns>
+        private bool CheckMatch(char initial, char final)
+        {
+            try
+            {
+                var closure = closingCharsDictionary[initial];
+
+                return (final == closure);
+            }
+            catch
+            {
+                return false;
+            }
         }
     }
 
@@ -109,6 +142,7 @@ namespace ParametersValidation
 
             Assert.IsFalse(result);
         }
+
         [TestMethod]
         public void IncompleteClosing()
         {
